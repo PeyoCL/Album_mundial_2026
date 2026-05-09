82bf834d8da5132fa81d7f121c3842fd51a5dc25
PeyoCL <63672870+PeyoCL@users.noreply.github.com>
2026-05-09 10:21:05 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 1. CAMBIA ESTE NOMBRE CADA VEZ QUE ACTUALICES TU APP (v1, v2, v3, etc.)
-const CACHE_NAME = 'album-2026-v2'; 
+// Aumentamos a la Versión 3
+const CACHE_NAME = 'album-2026-v3'; 
 
 const urlsToCache = [
   './',
@@ -11,9 +11,7 @@ const urlsToCache = [
   './icon.svg'
 ];
 
-// Instalar y guardar en caché
 self.addEventListener('install', event => {
-  // Obliga al Service Worker a instalarse inmediatamente
   self.skipWaiting(); 
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -21,7 +19,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// Activar y limpiar cachés antiguos
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
@@ -29,18 +26,15 @@ self.addEventListener('activate', event => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            // Si el caché antiguo no es el actual, lo borra
             return caches.delete(cacheName); 
           }
         })
       );
     })
   );
-  // Toma el control de la página inmediatamente
   return self.clients.claim(); 
 });
 
-// Interceptar peticiones
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
