8a624909e888b57f1f18b11e8bf640ef07bf1753
PeyoCL <63672870+PeyoCL@users.noreply.github.com>
2026-05-16 22:06:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'album-2026-v35'; 
+const CACHE_NAME = 'album-2026-v36'; 
 
 const urlsToCache = [
   './',
   './index.html',
-  './style.css?v=35',
-  './app.js?v=35',
-  './data.js?v=35',
+  './style.css?v=36',
+  './app.js?v=36',
+  './data.js?v=36',
   './manifest.json',
   './icon.svg'
 ];
