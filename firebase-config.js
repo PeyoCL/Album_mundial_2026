708311bf3ef006cb85a6289d1f6a4e697f11e9c8
PeyoCL <63672870+PeyoCL@users.noreply.github.com>
2026-06-04 11:01:03 -0400

    firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// firebase-config.js - Conexión con la nube
-
-// Importamos la versión 12.14.0 que te asignó Firebase
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app-check.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
 
-// Tu configuración real
 const firebaseConfig = {
   apiKey: "AIzaSyADrfxXPL5Cbh67NSlozPlJ81CNtT81PpI",
   authDomain: "album-2026-b734a.firebaseapp.com",
@@ -16,13 +13,15 @@ const firebaseConfig = {
   measurementId: "G-NJVZ2YYV9R"
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LfEywwtAAAAADC9T77QS5btZpRnEAdIffC71GFu'),
+  isTokenAutoRefreshEnabled: true 
+});
 
-// Inicializamos los servicios
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
 
-// Exportamos las herramientas para usarlas en tu app.js
+
 export { signInWithPopup, signOut, onAuthStateChanged, doc, setDoc, getDoc };
